2715166e09688c8e928c2577f69c78381e4e6775
Zach Rizzo <rizzozachary2@gmail.com>
2024-08-27 08:22:36 -0500

    added ignore node_modules

--- init.lua
+++ init.lua
@@ -450,6 +450,11 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          file_ignore_patterns = {
+            'node_modules',
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
