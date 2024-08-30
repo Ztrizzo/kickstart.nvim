c5784b373aae9cb95ab3057802c8749bb53ab871
Zach Rizzo <rizzozachary2@gmail.com>
2024-08-29 23:53:31 -0500

    added apex filetype mappings

--- init.lua
+++ init.lua
@@ -204,6 +204,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.filetype.add {
+  extension = {
+    cls = 'apex',
+    apex = 'apex',
+    trigger = 'apex',
+  },
+}
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
