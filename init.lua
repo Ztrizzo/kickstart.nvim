4d13e9828859f5d7de344c87505c81bc9a4da1fa
Zachary Rizzo <rizzoZachary2@gmail.com>
2024-09-06 22:33:09 -0500

    removed salesforce extensions because sf cli doesnt work with wls

--- init.lua
+++ init.lua
@@ -75,14 +75,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Load salesforce org info when nvim opens
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = vim.api.nvim_create_augroup('salesforce', { clear = true }),
-  callback = function()
-    require('salesforce.org_manager'):get_org_info()
-  end,
-})
-
 -- Add Apex as a filetype
 vim.filetype.add {
   extension = {
@@ -113,99 +105,6 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- {
-  --   'nvim-lualine/lualine.nvim',
-  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  --   config = function()
-  --     require('lualine').setup {
-  --       sections = {
-  --         lualine_c = {
-  --           'filename',
-  --           {
-  --             "require'salesforce.org_manager':get_default_alias()",
-  --             icon = 'DEFAULT ORG',
-  --           },
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
-  -- Salesforce config
-  {
-    'jonathanmorris180/salesforce.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-
-    config = function()
-      require('salesforce').setup {
-        debug = {
-          to_file = true,
-        },
-      }
-      -- require('salesforce.org_manager'):get_default_alias()
-      -- require('salesforce.org_manager'):get_org_info()
-    end,
-    keys = {
-      {
-        '<leader>oT',
-        function()
-          require('salesforce.test_runner'):execute_current_class()
-        end,
-        desc = 'Salesforce [O]rg execute all [T]ests in class',
-      },
-      {
-        '<leader>ot',
-        function()
-          require('salesforce.test_runner'):execute_current_method()
-        end,
-        desc = 'Salesforce [O]rg execute [T]est at cursor',
-      },
-      {
-        '<leader>oa',
-        function()
-          require('salesforce.component_generator'):create_apex()
-        end,
-        desc = 'Salesforce [O]rg create Apex',
-      },
-      {
-        '<leader>ol',
-        function()
-          require('salesforce.component_generator'):create_lightning_component()
-        end,
-        desc = 'Salesforce [O]rg create Lightning Component',
-      },
-      {
-        '<leader>or',
-        function()
-          require('salesforce.file_manager'):pull_from_org()
-        end,
-        desc = '[O]rg [R]etrieve',
-      },
-      {
-        '<leader>od',
-        function()
-          require('salesforce.org_manager'):set_default_org()
-        end,
-        desc = '[O]rg set [D]efault',
-      },
-      {
-        '<leader>op',
-        function()
-          require('salesforce.file_manager'):push_to_org()
-        end,
-        desc = '[O]rg [P]ush',
-      },
-      {
-        '<leader>oo',
-        function()
-          vim.fn.jobstart 'sf org open'
-        end,
-        desc = '[O]rg [O]pen',
-      },
-    },
-  },
   -- Harpoon config
   {
     'theprimeagen/harpoon',
